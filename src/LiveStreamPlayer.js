777b957858ccf1f7abcf037224af43b369174454
Grubenko Maxim <mdgrubenko@maieducation>
2023-05-16 18:27:22 +0300

    add https stream on ios

--- src/LiveStreamPlayer.js
+++ src/LiveStreamPlayer.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import Hls from 'hls.js';
 import VideoPlayer from './VideoPlayer';
 
 const Livestream = () => {
@@ -7,28 +6,42 @@ const Livestream = () => {
 
   useEffect(() => {
     const videoElement = videoRef.current;
-    const hls = new Hls();
 
-    if (Hls.isSupported()) {
+    if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+      videoElement.src = 'https://filimonov.org/hls/stream.m3u8'; // Замените на ваш URL M3U8
+      videoElement.addEventListener('loadedmetadata', () => {
+        videoElement.play();
+      });
+    } else if (Hls.isSupported()) {
+      const hls = new Hls();
       hls.loadSource('https://filimonov.org/hls/stream.m3u8'); // Замените на ваш URL M3U8
       hls.attachMedia(videoElement);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
         videoElement.play();
       });
-    }
 
-    return () => {
-      if (hls) {
-        hls.destroy();
-      }
-    };
+      return () => {
+        if (hls) {
+          hls.destroy();
+        }
+      };
+    }
   }, []);
 
-  if (!Hls.isSupported()) {
+  if (!videoRef.current || (!videoRef.current.canPlayType('application/vnd.apple.mpegurl') && !Hls.isSupported())) {
     return <VideoPlayer videoId="bxynzesjB6E" />;
   }
 
-  return <video ref={videoRef} style={{ width: '100%' }} controls />;
+  return (
+    <video
+      ref={videoRef}
+      style={{ width: '100%' }}
+      controls
+      playsInline
+      autoPlay
+      muted // добавлено для автовоспроизведения на мобильных устройствах
+    />
+  );
 };
 
 export default Livestream;
